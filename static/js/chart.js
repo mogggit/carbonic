2f02d5b48d819db59ce63d0eaf03b8262a67988b
mogggit <moggit2023@gmail.com>
2022-07-24 22:29:54 +0800

    Added chart

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,25 +1,23 @@
+$(window).resize(function(){
+    drawChart();
+});
 
-// 圓餅圖
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-    console.log("is this loading")
-
     var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['Work',     11],
-        ['Eat',      2],
-        ['Commute',  2],
-        ['Watch TV', 2],
-        ['Sleep',    7]
+        ['Type', 'kg of carbon'],
+        ['Bus', 11],
+        ['Car', 20],        
     ]);
 
     var options = {
-        title: 'My Daily Activities'
+        legend: {position: 'top', textStyle: {color: 'black', fontSize: 14}},
+        tooltip: {textStyle: {color: 'gray', fontSize: 16}, showColorCode: true}
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
     chart.draw(data, options);
-    }
+}
